a3479307cb1bb0414009c9b1302166358f62edaf
Yeshwanth Reddy <yeshwanthreddycherla@gmail.com>
2026-02-10 03:39:25 +0530

    Remove /api prefix from LevelController for consistency

--- src/main/java/com/leetquery/backend/controller/LevelController.java
+++ src/main/java/com/leetquery/backend/controller/LevelController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/levels")
+@RequestMapping("/levels")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class LevelController {
