46d212beab1c6c877d63cfa62dab2222bc77332a
Yeshwanth Reddy <yeshwanthreddycherla@gmail.com>
2026-02-09 20:35:28 +0530

    Add JdbcTemplate bean to DatabaseConfig

--- src/main/java/com/leetquery/backend/config/DatabaseConfig.java
+++ src/main/java/com/leetquery/backend/config/DatabaseConfig.java
@@ -2,6 +2,7 @@ package com.leetquery.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -29,4 +30,10 @@ public class DatabaseConfig {
         
         return dataSource;
     }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
+
